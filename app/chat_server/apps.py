4e82565bcb9069d6d12bc6fdd6cefdb6470de754
Edward Jimenez <edward.jimenez.m@gmail.com>
2019-12-20 21:10:09 -0500

    #2 Add the initial project structure includding the chat_server application

---
+++ app/chat_server/apps.py
@@ -0,0 +1,11 @@
+"""
+app.chat_server.apps
+--------------------
+Apps for chat_server app
+"""
+
+from django.apps import AppConfig
+
+
+class ChatServerConfig(AppConfig):
+    name = 'app.chat_server'
---
+++ app/chat_server/views.py
@@ -0,0 +1,8 @@
+"""
+app.chat_server.views
+---------------------
+Views for chat_server app
+"""
+from django.shortcuts import render
+
+# Create your views here.
